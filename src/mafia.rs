672c5a02b4b0e1f3bb725674f062635849c3cc07
jamescarterbell <jamescarterbell@gmail.com>
2019-10-22 16:03:45 -0400

    Can return port and begin listening at same time now

--- src/mafia.rs
+++ src/mafia.rs
@@ -1,64 +1,84 @@
-use std::net::TcpStream;
-use std::net::TcpListener;
 use rocket::State;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::sync::Arc;
 use std::sync::RwLock;
 use std::thread;
+use std::thread::JoinHandle;
 
 // Players have a role, a last vote, and a number
-struct Player{
+struct Player {
     role: Role,
     status: Status,
-    id: u8
+    id: u8,
 }
 
-enum Status{
+enum Status {
     Alive,
-    Dead
+    Dead,
 }
 
-enum Role{
+enum Role {
     Innocent,
     Detective,
     Doctor,
-    Wolf
+    Wolf,
 }
 
-struct ConnectedPlayer{
-    stream: RwLock<TcpStatus>,
-    player: Option<Player>
+struct ConnectedPlayer {
+    stream: TcpStatus,
+    player: Option<Player>,
 }
 
-enum TcpStatus{
-    Listening(TcpListener),
-    Connected(TcpStream)
+enum TcpStatus {
+    Uninitialized(TcpListener),
+    Listening(JoinHandle<Result<TcpStream, TcpListener>>),
+    Connected(TcpStream),
 }
 
-impl ConnectedPlayer{
-    fn open_connections<'a>(&mut self){
-        thread::spawn(move || {
-            let mut stream = self.stream.write().unwrap();
-            if let TcpStatus::Listening(listener) = &*stream{
-                if let Result::Ok((out_stream, _addr)) = listener.accept(){
-                    *stream = TcpStatus::Connected(out_stream)
+impl ConnectedPlayer {
+    fn open_connections(mut self) -> Self {
+        if let TcpStatus::Uninitialized(listener) = self.stream {
+            self.stream = TcpStatus::Listening(thread::spawn(|| ConnectedPlayer::listen(listener)));
+        }
+        self
+    }
+
+    fn listen(stream: TcpListener) -> Result<TcpStream, TcpListener> {
+        if let Result::Ok((out_stream, _addr)) = stream.accept() {
+            return Result::Ok(out_stream);
+        }
+        Result::Err(stream)
+    }
+
+    fn check_connections(mut self) -> Result<Self, ()> {
+        if let TcpStatus::Listening(handle) = self.stream {
+            let result = handle.join().unwrap();
+            match result {
+                Ok(stream) => {
+                    self.stream = TcpStatus::Connected(stream);
+                    return Result::Ok(self);
                 }
+                Err(listener) => self.stream = TcpStatus::Uninitialized(listener),
             }
-        });
+        }
+        Result::Err(())
     }
 }
 
 #[get("/new_connection")]
-pub fn new_connection(game_list: State<GameList>) -> String{
+pub fn new_connection(game_list: State<Arc<GameList>>) -> String {
     let mut games = game_list.inner().games.write().unwrap();
-    for game in games.iter_mut(){
-        if game.players.len() < game.max_players as usize{
-
+    for game in games.iter_mut() {
+        if game.players.len() < game.max_players as usize {
             let listener = TcpListener::bind("localhost:0").unwrap();
             let port = listener.local_addr().unwrap().port().to_string();
 
-            let new_player = ConnectedPlayer{
-                stream: RwLock::new(TcpStatus::Listening(listener)),
+            let new_player = ConnectedPlayer {
+                stream: TcpStatus::Uninitialized(listener),
                 player: None,
-            };
+            }
+            .open_connections();
             game.players.push(new_player);
             return port;
         }
@@ -67,17 +87,18 @@ pub fn new_connection(game_list: State<GameList>) -> String{
     let listener = TcpListener::bind("localhost:0").unwrap();
     let port = listener.local_addr().unwrap().port().to_string();
 
-    let new_player = ConnectedPlayer{
-        stream: RwLock::new(TcpStatus::Listening(listener)),
+    let new_player = ConnectedPlayer {
+        stream: TcpStatus::Uninitialized(listener),
         player: None,
-    };
+    }
+    .open_connections();
 
     game.players.push(new_player);
     games.push(game);
     return port;
 }
 
-struct Game{
+pub struct Game {
     players: Vec<ConnectedPlayer>,
     phase: Phase,
     mafia_left: u8,
@@ -85,9 +106,9 @@ struct Game{
     max_players: u8,
 }
 
-impl Game{
-    pub fn new(max_players: u8) -> Game{
-        Game{
+impl Game {
+    pub fn new(max_players: u8) -> Game {
+        Game {
             players: vec![],
             phase: Phase::Start,
             mafia_left: 0,
@@ -97,23 +118,23 @@ impl Game{
     }
 }
 
-enum Phase{
+enum Phase {
     Start,
-    PreVote,
     Detect,
+    PreVote,
     Vote,
     Save,
     Kill,
 }
 
-pub struct GameList{
-    games: RwLock<Vec<Game>>
+pub struct GameList {
+    pub games: RwLock<Vec<Game>>,
 }
 
-impl GameList{
-    pub fn new() -> GameList{
-        GameList{
-            games: RwLock::new(vec![])
+impl GameList {
+    pub fn new() -> GameList {
+        GameList {
+            games: RwLock::new(vec![]),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod mafia;
 
+use std::sync::Arc;
+use std::thread;
+
 fn main() {
-    let game_list = mafia::GameList::new();
-    rocket::ignite().manage(game_list).mount("/", routes![mafia::new_connection]).launch();
+    let game_list = Arc::new(mafia::GameList::new());
+    let in_list = game_list.clone();
+    let t = thread::spawn(move || loop {
+        println!("{}", in_list.games.read().unwrap().len())
+    });
+    rocket::ignite()
+        .manage(game_list)
+        .mount("/", routes![mafia::new_connection])
+        .launch();
 }
