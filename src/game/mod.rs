28ecec994e6f95cc4eafd0ea71a871ba13fb2a54
jamescarterbell@gmail.com <jamescarterbell@gmail.com>
2019-10-24 22:26:59 -0400

    So close, need to fix Connector

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,26 +1,145 @@
 pub mod connected_player;
-pub mod game_trait;
 
-use connected_player::*;
+pub use connected_player::*;
+use rocket::http::Method;
 use rocket::*;
+use std::io::Cursor;
 use std::marker::Send;
-use std::sync::mpsc;
+use std::sync::mpsc::*;
 use std::sync::Mutex;
+use std::thread::JoinHandle;
 use ws::*;
 
-#[get("/new_connection")]
-pub fn new_connection<T>(player_channel: State<Mutex<mpsc::Sender<ConnectedPlayer<T>>>>) -> String
+pub fn new_connection<P>(player_channel: &std::sync::mpsc::Sender<ConnectedPlayer<P>>) -> String
 where
-    T: Player + Send,
+    P: Player + Send + 'static,
 {
-    let mut port: String;
+    let mut port = "00000".to_string();
 
-    let new_player = ConnectedPlayer::new();
+    let mut new_player = ConnectedPlayer::new();
     if let SocketStatus::Uninitialized(socket) = new_player.socket {
-        new_player.socket = SocketStatus::Uninitialized(socket.bind("127.0.0.1:00000").unwrap());
+        let socket = socket.bind("127.0.0.1:00000").unwrap();
         port = socket.local_addr().unwrap().port().to_string();
+        new_player.socket = SocketStatus::Uninitialized(socket);
     }
 
-    let _ = player_channel.lock().unwrap().send(new_player);
+    let _ = player_channel.send(new_player);
     return port;
+}
+
+#[derive(Clone)]
+pub struct Connector<P>(std::sync::mpsc::Sender<ConnectedPlayer<P>>)
+where
+    P: Send + Player + 'static;
+
+impl<P> rocket::Handler for Connector<P>
+where
+    P: Send + Player + Clone + 'static,
+{
+    fn handle<'r>(
+        &self,
+        _req: &'r rocket::Request,
+        _data: Data,
+    ) -> rocket::Outcome<rocket::Response<'r>, rocket::http::Status, rocket::Data> {
+        rocket::Outcome::Success({
+            let mut res = rocket::Response::new();
+            res.set_sized_body(Cursor::new(new_connection(&self.0)));
+            res
+        })
+    }
+}
+
+unsafe impl<P> std::marker::Sync for Connector<P> where P: Send + Player {}
+
+pub fn launch<P, G, C>()
+where
+    P: Player + Send + 'static,
+    G: Game<P> + Send + 'static,
+{
+    let (send_games, recieve_games) = channel::<G>();
+    let (send_players, recieve_players) = channel::<ConnectedPlayer<P>>();
+
+    check_games::<P, G>(send_games, recieve_players);
+    run_active_games::<P, G>(recieve_games, send_players.clone());
+    let route = Route::new(Method::Get, "/new_connection", Connector(send_players));
+    rocket::ignite().mount("/", vec![route]).launch();
+}
+
+fn check_games<P, G>(
+    out: std::sync::mpsc::Sender<G>,
+    players: Receiver<ConnectedPlayer<P>>,
+) -> JoinHandle<()>
+where
+    P: Player + Send + 'static,
+    G: Game<P> + Send + 'static,
+{
+    std::thread::spawn(move || {
+        let mut game: Option<G> = None;
+        loop {
+            for player in players.try_iter() {
+                match game {
+                    Some(mut lobby) => {
+                        let players = lobby.player_list_mut();
+                        players.push(player);
+                        if players.len() == lobby.max_players() as usize {
+                            let _ = out.send(lobby);
+                            game = None;
+                        } else {
+                            game = Some(lobby);
+                        }
+                    }
+                    None => {
+                        let mut lobby = G::new(8);
+                        lobby.player_list_mut().push(player);
+                        game = Some(lobby);
+                    }
+                }
+            }
+        }
+    })
+}
+
+pub fn run_active_games<P, G>(
+    input: Receiver<G>,
+    out_players: std::sync::mpsc::Sender<ConnectedPlayer<P>>,
+) -> JoinHandle<()>
+where
+    P: Player + Send + 'static,
+    G: Game<P> + Send + 'static,
+{
+    std::thread::spawn(move || {
+        let mut games: Vec<G> = Vec::new();
+        loop {
+            for game in input.try_iter() {
+                games.push(game);
+            }
+            let mut good_games = vec![];
+            for mut game in games.drain(..) {
+                game.run_game();
+                if game.over() {
+                    for mut player in game.player_list_mut().drain(..) {
+                        if !player.check_connections() {
+                            let _ = out_players.send(player);
+                        }
+                    }
+                } else {
+                    good_games.push(game);
+                }
+            }
+            games = good_games;
+        }
+    })
+}
+
+pub trait Game<P>
+where
+    P: connected_player::Player + std::marker::Send + 'static,
+{
+    fn new(players: usize) -> Self;
+    fn run_game(&mut self);
+    fn over(&self) -> bool;
+    fn player_list(&self) -> &Vec<ConnectedPlayer<P>>;
+    fn player_list_mut(&mut self) -> &mut Vec<ConnectedPlayer<P>>;
+    fn max_players(&self) -> usize;
+    fn max_players_mut(&mut self) -> &mut usize;
 }
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,126 @@ extern crate rocket;
 mod game;
 mod mafia;
 
+use game::{ConnectedPlayer, Connector, Game, Player};
+use rand::seq::SliceRandom;
 use std::sync::mpsc::channel;
 use std::sync::Mutex;
 
 fn main() {
-    let (send_games, recieve_games) = channel();
-    let (send_players, recieve_players) = channel();
-
-    mafia::check_games(send_games, recieve_players);
-    mafia::run_active_games(recieve_games, send_players.clone());
-    rocket::ignite()
-        .manage(Mutex::new(send_players))
-        .mount("/", routes![mafia::new_connection])
-        .launch();
+    game::launch::<MafiaPlayer, Mafia>();
 }
+
+pub struct Mafia {
+    players: Vec<ConnectedPlayer<MafiaPlayer>>,
+    phase: Phase,
+    mafia_left: usize,
+    innocent_left: usize,
+    max_players: usize,
+}
+
+impl Mafia {
+    fn create_role_vec(&self) -> Vec<Role> {
+        let mut roles = vec![];
+        roles.push(Role::Doctor);
+        roles.push(Role::Detective);
+        for _ in 0..self.max_players / 4 {
+            roles.push(Role::Mafia);
+        }
+        while roles.len() < self.max_players as usize {
+            roles.push(Role::Innocent);
+        }
+        roles
+    }
+}
+
+impl Game<MafiaPlayer> for Mafia {
+    fn new(max_players: usize) -> Mafia {
+        Mafia {
+            players: vec![],
+            phase: Phase::Start,
+            mafia_left: 0,
+            innocent_left: 0,
+            max_players: max_players,
+        }
+    }
+
+    fn run_game(&mut self) {
+        match self.phase {
+            Phase::Start => {
+                // Check if all players are connected
+                for player in self.players.iter_mut() {
+                    if !player.check_connections() {
+                        self.phase = Phase::End;
+                        return;
+                    }
+                }
+
+                // If all players are connected, create roles, and assign them to players
+                let mut roles = self.create_role_vec();
+                let mut rng = rand::thread_rng();
+                roles.shuffle(&mut rng);
+
+                for (i, player) in self.players.iter_mut().enumerate() {
+                    player.player = Some(MafiaPlayer {
+                        role: roles.pop().unwrap(),
+                        status: Status::Alive,
+                        id: i as u8,
+                    });
+                }
+                self.phase = Phase::Detect;
+            }
+            _ => println!("Phase not implemented"),
+        }
+    }
+
+    fn over(&self) -> bool {
+        self.phase == Phase::End
+    }
+
+    fn player_list(&self) -> &Vec<ConnectedPlayer<MafiaPlayer>> {
+        &self.players
+    }
+
+    fn player_list_mut(&mut self) -> &mut Vec<ConnectedPlayer<MafiaPlayer>> {
+        &mut self.players
+    }
+
+    fn max_players(&self) -> usize {
+        self.max_players
+    }
+
+    fn max_players_mut(&mut self) -> &mut usize {
+        &mut self.max_players
+    }
+}
+
+#[derive(Eq, PartialEq)]
+enum Phase {
+    Start,
+    Detect,
+    PreVote,
+    Vote,
+    Save,
+    Kill,
+    End,
+}
+
+struct MafiaPlayer {
+    role: Role,
+    status: Status,
+    id: u8,
+}
+
+enum Status {
+    Alive,
+    Dead,
+}
+
+enum Role {
+    Innocent,
+    Detective,
+    Doctor,
+    Mafia,
+}
+
+impl Player for MafiaPlayer {}
